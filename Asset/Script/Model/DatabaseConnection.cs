145529fc85fe51c2177562a021f049faa9082213
RizalUmami <mrizalumami393939@gmail.com>
2025-05-27 09:40:50 +0700

    Update Program DatabaseConnection

--- Asset/Script/Model/DatabaseConnection.cs
+++ Asset/Script/Model/DatabaseConnection.cs
@@ -1,1 +1,18 @@
+using MySql.Data.MySqlClient;
+
+public static class DatabaseConnection
+{
+    // Pastikan Anda menggunakan MySql.Data.dll versi 6.9.x atau Connector/NET terbaru
+private static readonly string connectionString =
+    "Server=localhost;" +
+    "Database=db_calistung;" +
+    "User ID=root;" +
+    "Password=;" +
+    "SSL Mode=None;";  // optional untuk lokal
+
+    public static MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+}
 
